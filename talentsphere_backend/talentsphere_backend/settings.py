b753f22b27fdc291e2144ee3717a81cf0e296d5c
Praveen Lokku <143445307+praveenlokku@users.noreply.github.com>
2025-04-15 09:05:37 +0530

    Update settings.py

--- talentsphere_backend/talentsphere_backend/settings.py
+++ talentsphere_backend/talentsphere_backend/settings.py
@@ -57,6 +57,7 @@ CHANNEL_LAYERS = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
