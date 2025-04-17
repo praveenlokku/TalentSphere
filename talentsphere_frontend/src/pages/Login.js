81f6f85868e916dd70c9b568a787cd1a7a4ed797
Praveen Lokku <143445307+praveenlokku@users.noreply.github.com>
2025-04-17 10:26:11 +0530

    Update Login.js

--- talentsphere_frontend/src/pages/Login.js
+++ talentsphere_frontend/src/pages/Login.js
@@ -10,7 +10,7 @@ function Login() {
 
     const handleLogin = async() => {
         try {
-            const response = await axios.post("http://127.0.0.1:8000/api/login/", {
+            const response = await axios.post("https://talentsphere-lbap.onrender.com/api/login/", {
                 email,
                 password,
             });
