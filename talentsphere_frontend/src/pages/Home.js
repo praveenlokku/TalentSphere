ad7f91b98523455b0e0ebc64c44ecf0fd87b0da9
Praveen Lokku <143445307+praveenlokku@users.noreply.github.com>
2025-04-17 12:25:42 +0530

    Update Home.js

--- talentsphere_frontend/src/pages/Home.js
+++ talentsphere_frontend/src/pages/Home.js
@@ -4,12 +4,10 @@ import { useNavigate } from "react-router-dom";
 function Home() {
     const navigate = useNavigate();
 
-    return (<Helmet>
-        <title>Stujya | Home</title>
-      </Helmet> <
+    return ( <
         div style = {
             { textAlign: "center", padding: "50px" }
-        } >
+        } ><Helmet><title>Stujya | Home</title></Helmet>
         <
         h1 > Welcome to TalentSphere < /h1> <
         p > Connect students with industrial projects. < /p> <
